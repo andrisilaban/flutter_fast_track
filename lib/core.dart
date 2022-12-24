8c46476d4bfd66cfd11d53a8ba2a97f104613c20
andrisilaban <andriantosilaban@gmail.com>
2022-12-24 08:00:25 +0700

    Add MVC Structure, config.json

---
+++ lib/core.dart
@@ -0,0 +1,12 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_fast_track/module/dashboard/controller/dashboard_controller.dart';
+export 'package:flutter_fast_track/module/dashboard/view/dashboard_view.dart';
+export 'package:flutter_fast_track/module/login/controller/login_controller.dart';
+export 'package:flutter_fast_track/module/login/view/login_view.dart';
+export 'package:flutter_fast_track/module/profile/controller/profile_controller.dart';
+export 'package:flutter_fast_track/module/profile/view/profile_view.dart';
+export 'state_util.dart';
